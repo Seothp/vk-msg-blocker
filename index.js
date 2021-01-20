26142e5802925a1946778153118f794296b3446c
Seothp <50671660+Seothp@users.noreply.github.com>
2021-01-20 15:07:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-'use strict'
-const delay = 1000 * 0.5;
-const blockId = 599480560;
-setInterval(
-  () => {
-    const messages = document.querySelectorAll(`div[data-peer="${blockId}"]`);
-    messages.forEach((e) => {
-      e.textContent = '';
-    })
-  }, 
-  delay,
-);
+// ==UserScript==
+// @name         vkMessageBlocker
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://vk.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    //  интервал проверки и удаления сообщений.
+    const delay = 1000 * 0.5;
+    // id пользователя в вк.
+    const blockId = 599480560;
+    setInterval(
+        () => {
+            const messages = document.querySelectorAll(`div[data-peer="${blockId}"]`);
+            messages.forEach((e) => {
+                e.textContent = '';
+            })
+        },
+        delay,
+    );
+})();
